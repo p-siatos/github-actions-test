1c6feaafb721c7164133d3177c452e1ea378f2fa
psiatos <panayiotis.siatos@gmail.com>
2024-07-08 16:27:11 +0300

    feat(17-3-custom-actions-docker) add validation in main.py

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,37 @@
+import requests as req
+from time import sleep
+import os
+
+def run():
+    url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+    url_reachable = ping_url(url, delay, max_trials)
+    
+    if not url_reachable:
+        raise Exception(f"Website {url} is malformed or unreachable")
+    
+    print(f"website {url} is reachable")
+
+
+def ping_url(url, delay, max_trials):
+    num_of_trials = 0
+    while num_of_trials < max_trials:
+        try:
+            res = req.get(url)
+            if res.status_code == 200:
+                print(f"Site {url} is reachable")
+                return True
+        except req.ConnectionError:
+            print(f'Website {url} is  unreachable. Retrying in {delay} seconds...')
+            sleep(delay)
+            num_of_trials += 1
+        except req.exceptions.MissingSchema:
+            print(f"Invalid Url format {url}")
+            return False
+
+    return False
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    run()
